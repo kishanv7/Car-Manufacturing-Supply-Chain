b99b6ba6c84b53bb10397a4bb1222206ee304ed2
kishanv7 <68203230+kishanv7@users.noreply.github.com>
2020-07-13 14:40:59 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ var router = express.Router();
  */
 var pmFront = require('../controllers/ProductManagmentController.js');
 
-// Auth API for Front User
+// PM API for Front User
 router.post('/api/front/v1/register-parts', pmFront.registerParts);
 router.post('/api/front/v1/get-parts', pmFront.getParts);
 router.post('/api/front/v1/get-part', pmFront.getPart);
+router.post('/api/front/v1/change-ownership-parts', pmFront.changeOwnershipParts);
+router.post('/api/front/v1/register-products', pmFront.registerProducts);
+router.post('/api/front/v1/get-products', pmFront.getProducts);
+router.post('/api/front/v1/get-product', pmFront.getProduct);
+router.post('/api/front/v1/change-ownership-products', pmFront.changeOwnershipProducts);
 
 module.exports = router;
